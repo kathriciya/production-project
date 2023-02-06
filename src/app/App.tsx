d3d9fe703064ec19522eb8827e00a7edfe8c2d5d
Ekaterina <Kata_Yurevna@mail.ru>
2023-02-07 01:25:48 +0700

    create  Navbar, UI Kit Link

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from 'app/providers/ThemeProvider';
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Navbar } from 'widgets/Navbar';
 import { AppRouter } from './providers/router';
 import './styles/index.scss';
 
@@ -8,10 +8,9 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>Toggle</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>Toggle</button>
     </div>
   );
 };

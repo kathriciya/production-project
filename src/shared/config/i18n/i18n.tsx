e464a1fe76909b9ed46a7669f419a47407e41309
Ekaterina <Kata_Yurevna@mail.ru>
2023-02-12 11:39:01 +0700

    update i18n

--- src/shared/config/i18n/i18n.tsx
+++ src/shared/config/i18n/i18n.tsx
@@ -11,6 +11,9 @@ i18n
     fallbackLng: false,
     debug: __IS_DEV__,
     interpolation: { escapeValue: false },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
   });
 
 export default i18n;

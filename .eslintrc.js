8585de1ade6cd9e7483ab1f3991d3c95aff1bed0
Ekaterina <Kata_Yurevna@mail.ru>
2023-02-24 16:46:35 +0700

    add modal, portal, change header

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'i18next'],
+  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
   rules: {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
@@ -33,14 +33,23 @@ module.exports = {
       'error',
       { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
     ],
-    'max-len': ['error', { code: 110, ignoreComments: true }],
+    'max-len': ['error', { code: 180, ignoreComments: true }],
     'comma-dangle': 1,
     'object-curly-newline': ['error', { multiline: true }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
+    'operator-linebreak': [
+      'error',
+      'after',
+      { overrides: { ':': 'before' } },
+    ],
   },
   globals: { __IS_DEV__: true },
   overrides: [
     {
-      files: ['**/src/**/*.test.{ts,tsx}'],
+      files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
       rules: { 'i18next/no-literal-string': 'off' },
     },
   ],

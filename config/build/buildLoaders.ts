ab38c69fb942a0454fa11ab572d7926070b945d6
Ekaterina <Kata_Yurevna@mail.ru>
2023-03-19 00:31:14 +0700

    add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,32 +1,16 @@
 import webpack from 'webpack';
 import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options;
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   };
 
-  const babelLoader = {
-    test: /\.(js|jsx|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['ru', 'en'],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
-      },
-    },
-  };
+  const babelLoader = buildBabelLoader(options);
 
   const cssLoader = buildCssLoader(isDev);
 
@@ -38,9 +22,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-    use: [
-      { loader: 'file-loader' },
-    ],
+    use: [{ loader: 'file-loader' }],
   };
 
   return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];

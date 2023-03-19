02efb03ec3279f17a7a8c3954b32fa310a719e22
Ekaterina <Kata_Yurevna@mail.ru>
2023-03-19 21:18:38 +0700

    add articles page

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,7 +6,7 @@ import { buildResolvers } from './buildResolvers';
 import { BuildOptions } from './types/config';
 
 export function buildWebpackConfig(
-  options: BuildOptions,
+  options: BuildOptions
 ): webpack.Configuration {
   const { paths, mode, isDev } = options;
 
@@ -17,6 +17,7 @@ export function buildWebpackConfig(
       filename: '[name].[contenthash].js',
       path: paths.build,
       clean: true,
+      publicPath: '/',
     },
     plugins: buildPlugins(options),
     module: { rules: buildLoaders(options) },

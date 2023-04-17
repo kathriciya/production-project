cc1a71d98c9a193564a37224180fcbe131483639
Ekaterina <Kata_Yurevna@mail.ru>
2023-04-18 05:02:44 +0700

    fix webpack storybook config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -9,6 +9,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
     html: '',
     entry: '',
     src: path.resolve(__dirname, '..', '..', 'src'),
+    locales: '',
+    buildLocales: '',
   };
 
   config.resolve!.modules!.push(paths.src);
@@ -22,7 +24,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
       }
 
       return rule;
-    },
+    }
   );
 
   config.module!.rules!.push({
@@ -37,7 +39,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
       __IS_DEV__: JSON.stringify(true),
       __API__: JSON.stringify(''),
       __PROJECT__: JSON.stringify('storybook'),
-    }),
+    })
   );
 
   return config;

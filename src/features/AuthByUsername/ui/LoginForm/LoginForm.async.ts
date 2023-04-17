dea792c66de0979d6637352d5689c44918afc693
Ekaterina <Kata_Yurevna@mail.ru>
2023-04-18 04:33:16 +0700

    add copyplugin, update api config

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
@@ -2,11 +2,5 @@ import { FC, lazy } from 'react';
 import { LoginFormProps } from './LoginForm';
 
 export const LoginFormAsync = lazy<FC<LoginFormProps>>(
-  () =>
-    // eslint-disable-next-line implicit-arrow-linebreak
-    new Promise((resolve) => {
-      // @ts-ignore
-      // Это временно!
-      setTimeout(() => resolve(import('./LoginForm.tsx')), 1500);
-    }),
+  () => import('./LoginForm')
 );
